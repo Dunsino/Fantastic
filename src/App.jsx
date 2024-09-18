af6d6b9f0074e492f09ec48fb561917625c25b03
Dunsin <58459950+Dunsino@users.noreply.github.com>
2024-09-18 20:53:07 +0100

    Initial upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+{
+  /*import React from "react";*/
+}
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <main className="overflow-x-hidden">
+      <Hero />
+      <Navbar />
+    </main>
+  );
+};
+
+export default App;
